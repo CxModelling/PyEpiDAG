d6256246088d0965ea5183c69d33bc91bda8cebf
TimeWz667 <TimeWz667@gmail.com>
2018-03-25 17:12:44 +0100

    add PseudoLoci

--- epidag/bayesnet/loci.py
+++ epidag/bayesnet/loci.py
@@ -3,7 +3,7 @@ from epidag.bayesnet import parse_distribution, MATH_FUNC
 import ast
 
 __author__ = 'TimeWz667'
-__all__ = ['ValueLoci', 'ExoValueLoci', 'DistributionLoci', 'FunctionLoci',
+__all__ = ['ValueLoci', 'ExoValueLoci', 'DistributionLoci', 'FunctionLoci', 'PseudoLoci',
            'parse_parents']
 
 
@@ -20,6 +20,9 @@ def parse_parents(seq):
 
 
 class Loci(metaclass=ABCMeta):
+    def __call__(self, parent=None):
+        return self.sample(parent)
+
     @abstractmethod
     def sample(self, parent=None):
         pass
@@ -109,8 +112,11 @@ class DistributionLoci(Loci):
         return self.Parent
 
     def get_distribution(self, pas):
-        dd = dict(pas)
-        return parse_distribution(self.Func, glo=MATH_FUNC, loc=dict(pas))
+        try:
+            pas = dict(pas)
+        except TypeError:
+            pas = dict()
+        return parse_distribution(self.Func, glo=MATH_FUNC, loc=pas)
 
     def sample(self, pas=None):
         return self.get_distribution(pas).sample()
@@ -147,7 +153,7 @@ class FunctionLoci(Loci):
         try:
             return eval(self.Func, MATH_FUNC, pas)
         except NameError:
-            return eval(self.Func, MATH_FUNC, dict(pas))
+            return eval(self.Func, MATH_FUNC, dict(pas) if pas else dict())
 
     def evaluate(self, pas=None):
         return 0
@@ -164,15 +170,54 @@ class FunctionLoci(Loci):
     __str__ = __repr__
 
 
+class PseudoLoci(Loci):
+    def __init__(self, name, val, pas=None):
+        self.Name = name
+        self.Func = val
+        if pas:
+            self.Parent = pas
+        else:
+            self.Parent, _ = parse_parents(val)
+        self.Func = 'f(' + ', '.join(self.Parents) + ')'
+
+    @property
+    def Parents(self):
+        return self.Parent
+
+    def sample(self, pas=None):
+        raise AttributeError('Pseudo node can not be implemented')
+
+    def evaluate(self, pas=None):
+        raise AttributeError('Pseudo node can not be evaluated')
+
+    def fill(self, gene):
+        raise AttributeError('Pseudo node can not be implemented')
+
+    def to_json(self):
+        return {'Type': 'Pseudo', 'Def': self.Func, 'Parents': self.Parents}
+
+    def __repr__(self):
+        return '{} = {}'.format(self.Name, self.Func)
+
+    __str__ = __repr__
+
+
 if __name__ == '__main__':
     d1 = '1/0.01 + exp(k) + u'
-    d1 = FunctionLoci('ss', d1)
+    d1 = FunctionLoci('s1', d1)
     print(d1.Parents)
     print(d1.sample({'k': 2, 'u': 5}))
     print(d1.to_json())
 
     d2 = 'gamma(cos(1/0.01), ss)'
-    d2 = DistributionLoci('sss', d2)
+    d2 = DistributionLoci('s2', d2)
     print(d2.Parents)
     print(d2.sample({'ss': 10, 'u': 5}))
     print(d2.to_json())
+
+    d3 = '1/0.01 + exp(k) + u'
+    d3 = PseudoLoci('s3', d3)
+    print(d3.Parents)
+    print(d3.Func)
+    print(d3.to_json())
+
